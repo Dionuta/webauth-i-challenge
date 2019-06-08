196032310fbce89eede4d896a9703c44b714083f
Dionuta <diounta@gmail.com>
2019-06-07 18:35:57 -0700

    mvp

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+
+const db = require("./data/dbConfig.js");
+const Users = require("./users/users-model.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+
+  if (!user.username || user.password0) {
+    return res.status(500).json({ message: "Need username and password" });
+  }
+
+  const hash = bcrypt.hashSync(user.password, 16);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function authorize(req, res, next) {
+  const username = req.headers["x-username"];
+  const password = req.headers["x-password"];
+
+  if (!username || !password) {
+    return res.status(500).json({ message: "Need username and password" });
+  }
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+
+server.get("/api/users", authorize, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
